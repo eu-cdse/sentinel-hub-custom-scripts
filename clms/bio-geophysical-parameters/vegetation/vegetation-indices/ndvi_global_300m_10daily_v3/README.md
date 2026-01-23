cc1676b99f61b1b6dad4f7cc8417d40527d88745
Blaž Pavlica <blaz.pavlica@sinergise.com>
2026-01-23 14:44:47 +0100

    added sce, lswt, ndvi, fapar and lai

---
+++ clms/bio-geophysical-parameters/vegetation/vegetation-indices/ndvi_global_300m_10daily_v3/README.md
@@ -0,0 +1,3 @@
+#  Normalised Difference Vegetation Index 2020-present (raster 300 m), global, 10-daily – version 2
+
+Normalized Difference Vegetation Index (NDVI) is an indicator of the greenness of the biomes. Every 10-days estimates are available in near real time at global scale in the spatial resolution of about 300 m from January 2014 to the present.
---
+++ clms/bio-geophysical-parameters/vegetation/vegetation-properties/fapar_global_300m_10daily_v2/scripts/fapar.js
@@ -0,0 +1,49 @@
+//VERSION=3
+const factor = 1 / 250;
+const offset = 0;
+
+function setup() {
+  return {
+    input: ["FAPAR", "dataMask"],
+    output: [
+      { id: "default", bands: 4, sampleType: "UINT8" },
+      { id: "index", bands: 1, sampleType: "FLOAT32" },
+      { id: "eobrowserStats", bands: 1, sampleType: "FLOAT32" },
+      { id: "dataMask", bands: 1 },
+    ],
+  };
+}
+
+function evaluatePixel(samples) {
+  var originalValue = samples.FAPAR;
+
+  let val = originalValue * factor + offset;
+
+  let dataMask = samples.dataMask;
+
+  const indexVal = dataMask === 1 ? val : NaN;
+  const imgVals = visualizer.process(val);
+
+  return {
+    default: imgVals.concat(dataMask * 255),
+    index: [indexVal],
+    eobrowserStats: [val],
+    dataMask: [dataMask],
+  };
+}
+
+const ColorBar = [
+  [0.0, [140, 92, 8]],
+  [0.1, [174, 141, 14]],
+  [0.2, [209, 190, 21]],
+  [0.3, [243, 239, 27]],
+  [0.4, [229, 239, 26]],
+  [0.5, [190, 216, 22]],
+  [0.6, [152, 192, 17]],
+  [0.7, [114, 169, 13]],
+  [0.8, [76, 146, 8]],
+  [0.9, [38, 123, 4]],
+  [0.94, [23, 114, 2]],
+  [1.02, [255, 255, 255]],
+];
+const visualizer = new ColorRampVisualizer(ColorBar);
